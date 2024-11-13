cecd7d39c7195c9f8b8268c2a81bf82472713dc7
LeoMasago <leo.masago@etec.sp.gov.br>
2024-11-13 18:20:03 -0300

    ajustando header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,12 +10,12 @@ export default function Header() {
     return (
         <>
             <header>
-                <div className="flex items-center justify-between">
+                <div className="flex items-center w-full justify-around">
                     <div>
                         <h1 className="font-bold">Nome do Site</h1>
                     </div>
-                    <nav>
-                        <ul className="md:flex gap-5 hidden">
+                    <nav className="">
+                        <ul className="md:flex gap-4 hidden">
                             <li><NavLink to="/">Home</NavLink></li>
                             <li><NavLink to="/informacoes">Informações</NavLink></li>
                             <li><NavLink to="/sobre">Sobre</NavLink></li>
@@ -31,7 +31,7 @@ export default function Header() {
                 </div>
             </header>
 
-            <div id="menu" className="md:hidden bg-purple-50 w-full"></div>
+            <div id="menu" className="md:hidden bg-purple-50 w-full">
                 <div className="px-2 pt-2 pd-3 flex flex-col items-center">
                     <a className="hover:underline transition duration-300 mt-3 hover:scale-[1.07] flex gap-2">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
@@ -44,19 +44,20 @@ export default function Header() {
                             <path stroke-linecap="round" stroke-linejoin="round" d="M9 17.25v1.007a3 3 0 0 1-.879 2.122L7.5 21h9l-.621-.621A3 3 0 0 1 15 18.257V17.25m6-12V15a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 15V5.25m18 0A2.25 2.25 0 0 0 18.75 3H5.25A2.25 2.25 0 0 0 3 5.25m18 0V12a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 12V5.25" />
                         </svg>
 
-                        <NavLink to="/projetos">Informações</NavLink></a>
+                        <NavLink to="/informacoes">Informações</NavLink></a>
                     <a className="hover:underline transition duration-300 mt-3 hover:scale-[1.07] flex gap-2">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M9 17.25v1.007a3 3 0 0 1-.879 2.122L7.5 21h9l-.621-.621A3 3 0 0 1 15 18.257V17.25m6-12V15a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 15V5.25m18 0A2.25 2.25 0 0 0 18.75 3H5.25A2.25 2.25 0 0 0 3 5.25m18 0V12a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 12V5.25" />
                         </svg>
-                        <NavLink to="/projetos">Sobre</NavLink></a>
+                        <NavLink to="/sobre">Sobre</NavLink></a>
 
                     <a className="hover:underline transition duration-300 mt-3 mb-3 hover:scale-[1.07] flex gap-2">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M4.26 10.147a60.438 60.438 0 0 0-.491 6.347A48.62 48.62 0 0 1 12 20.904a48.62 48.62 0 0 1 8.232-4.41 60.46 60.46 0 0 0-.491-6.347m-15.482 0a50.636 50.636 0 0 0-2.658-.813A59.906 59.906 0 0 1 12 3.493a59.903 59.903 0 0 1 10.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.717 50.717 0 0 1 12 13.489a50.702 50.702 0 0 1 7.74-3.342M6.75 15a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm0 0v-3.675A55.378 55.378 0 0 1 12 8.443m-7.007 11.55A5.981 5.981 0 0 0 6.75 15.75v-1.5" />
                         </svg>
-                        <NavLink to="/formacao">Contato</NavLink></a>
+                        <NavLink to="/contato">Contato</NavLink></a>
                 </div>
+            </div>
             
         </>
     )
